a29bca9ee23d5d5f85fa329589b120a48050ec3e
JH <huang0099@sbcglobal.net>
2021-01-03 20:47:32 -0800

    jquery with nav

--- resources/js/script.js
+++ resources/js/script.js
@@ -89,4 +89,11 @@ $(document).ready(function() {
     });
 
 
+
+  /* mobile navigation */
+  $('.js--nav-icon').click(function(){
+    var nav = $('.js--main-nav');
+    nav.slideToggle(200);
+  });
+
 });
